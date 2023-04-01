7629e1131da729d09d60cb0f4b3b5df85f10f110
ny248 <69338752+ny248@users.noreply.github.com>
2023-04-01 14:23:11 +0900

    Notation fixed
    Corrected some functions that did not have a consistent notation.

--- library.py
+++ library.py
@@ -1,48 +1,37 @@
-class queue_elem:
-    def __init__(self):
-        self.next = None
-        self.prev = None
-        self.value = None
+# bi-directional queue
+# It considers an array as a circle
+# add(value, direction)
 class queue:
     def __init__(self):
-        self.begin = queue_elem()
-        self.end = queue_elem()
-        self.begin.next = self.end
-        self.end.pref = self.begin
-    def empty(self):
-        return self.begin.next == self.end
-    def add_left(self, value):
-        elem = queue_elem()
-        elem.prev = self.begin
-        elem.next = self.begin.next
-        self.begin.next.prev = elem
-        self.begin.next = elem
-        elem.value = value
-    def add_right(self, elem):
-        elem = queue_elem()
-        elem.prev = self.end.prev
-        elem.next = self.end
-        self.end.prev.next = elem
-        self.end.prev = elem
-        elem.value = value
-    def pop_left(self):
-        if self.empty():
+        self.values = [None]
+        self.size = 1
+        self.begin = 0
+		self.N = 0
+	def expand(self):
+		self.values.extend(self.values)
+		self.size *= 2
+	# direction  0 : add element to the left, 1 : add element to the right
+    def add(self, value, direction = 1):
+        if self.N == self.size:
+			self.extend()
+		if direction == 0:
+			self.begin = (self.begin - 1) % self.size
+			self.values[self.begin] = value
+		else:
+			self.values[(self.begin + self.N) % self.size] = value
+		self.N += 1
+    def pop(self, direction = 1):
+        if self.N == 0:
             return None
-        ret = self.begin.next.value
-        self.begin.next = self.begin.next.next
-        del self.begin.next.prev
-        self.begin.next.prev = self.begin
-        return ret
-    def pop_right(self):
-        if self.empty():
-            return None
-        ret = self.end.prev.value
-        self.end.prev = self.end.prev.prev
-        del self.end.prev.next
-        self.end.prev.next = self.end
-        return ret
+		self.N -= 1
+		if direction == 0:
+			self.begin = (self.begin + 1) % self.size
+        	return self.values[(self.begin - 1) % self.size]
+		else:
+			return self.values[(self.begin + self.N) % self.size]
 
-class mod_int: #素数を法とした自然数, または有理数を扱うことのできるクラス
+# Handle natural numbers with the law of primes, or rational numbers
+class mod_int:
     def __init__(self, value, mod):
         self.value = value % mod
         self.mod = mod
@@ -78,8 +67,39 @@ class mod_int: #素数を法とした自然数, または有理数を扱うこ�
         return mod_int(ret, self.mod)
     def __repr__(self):
         return str(self.value)
-A = mod_int(3, 998244353)
-print(A / 6)
+	
+
+class segment_tree:
+    def __init__(self, A, unit, func):
+        self.N = 1
+        self.unit = unit
+        self.func = func
+        while self.N < len(A):
+            self.N *= 2
+        self.memo = [None for i in range(self.N * 2 - 1)]
+        for i in range(len(A)):
+            self.memo[i + self.N - 1] = A[i]
+		for i in range(len(A), self.N):
+			self.memo[i + self.N - 1] = self.unit
+        for i in range(self.N - 2, -1, -1):
+            self.memo[i] = self.func(self.memo[2 * i + 1], self.memo[2 * i + 2])
+    def update(self, index, value):
+        i = index + self.N - 1
+        self.memo[i] = value
+        while i > 0:
+            i = (i - 1) // 2
+            self.memo[i] = self.func(self.memo[2 * i + 1],self.memo[2 * i + 2])
+    def get(self, begin, end):
+        b, e = begin + self.N - 1, end + self.N - 1
+        ans = self.unit
+        while b < e:
+            if b % 2 == 0:
+                ans = self.func(ans, self.memo[b])
+            b = b // 2
+            if e % 2 == 0:
+                ans = self.func(ans, self.memo[e - 1])
+            e = (e - 1) // 2
+        return ans
 
 # 入力:大きさ2 ** N の配列 A[i]
 # 出力:大きさ2 ** N の配列 B[i] B[i] は i | j = i　となるような全てのjに対してのA[j]の和
@@ -133,39 +153,6 @@ def inv_fourier(A, N):
     return B
 inv_fourier(fourier([1,2,3,4,5,6,7,8],3),3)
 
-class segment_tree:
-    def __init__(self,A,unit,func):
-        self.N=1
-        self.unit=unit
-        self.func=func
-        while self.N<len(A):
-            self.N*=2
-        self.memo=[unit for i in range(self.N*2-1)]
-        for i in range(len(A)):
-            self.memo[i+self.N-1]=A[i]
-        for i in range(self.N-2,-1,-1):
-            self.memo[i]=func(self.memo[2*i+1],self.memo[2*i+2])
-    def change(self,index,value):
-        i=index+self.N-1
-        self.memo[i]=value
-        while i>0:
-            i=(i-1)//2
-            self.memo[i]=self.func(self.memo[2*i+1],self.memo[2*i+2])
-    def get(self,begin,end):
-        b,e=begin+self.N-1,end+self.N-1
-        ans=self.unit
-        while b<e:
-            if b%2==0:
-                ans=self.func(ans,self.memo[b])
-            b=b//2
-            if e%2==0:
-                ans=self.func(ans,self.memo[e-1])
-            e=(e-1)//2
-        return ans
-A=segment_tree([1,6,1,4,6,3,3],0,lambda a,b:a+b)
-print(A.get(0,6))
-A.change(2,9)
-print(A.get(1,4))
 
 
 
@@ -299,13 +286,6 @@ class priorityQueue:
             cur=c[target]
         return ret
 
-q=priorityQueue(lambda a,b:a>=b,[1,6,1,3,6,2,24,52,1,1252,521])
-for i in range(11):
-    print(q.pop())
-    q.push(4)
-    print(q.data)
-
-import heapq
 class node:
     def __init__(self, con):
         self.con = con#つながっている辺の行き先と重みの配列
@@ -328,23 +308,7 @@ class directedWeightedGraph:
                 for i in self.nodes[cur[1]].con:
                     heapq.heappush(watch, [i[1] + ans[cur[1]], i[0]])
         return ans
-G=directedWeightedGraph(3)
-G.connect(0,1,3)
-G.connect(1,2,4)
-G.connect(0,2,19)
-print(G.dijkstra(0))
 
-def p(a,p,mod):#素数体上の累乗
-    rem=p%(mod-1)
-    temp=a
-    ans=1
-    while rem:
-        if rem%2:
-            ans=(ans*temp)%mod
-        temp=(temp**2)%mod
-        rem//=2
-    return ans
-p(3,6,5)
 
 import heapq
 heap = []
